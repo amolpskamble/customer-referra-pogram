366cc9bba083c4d5463b312e580a7b9942f378b8
amolkamble <Amol227283_>
2018-08-20 04:23:22 +0530

    remove referral service

--- service/index.js
+++ service/index.js
@@ -1,9 +1,7 @@
 const Customer = require('./customer.service');
 const Ambassador = require('./ambassador.service');
-const Referral = require('./referral.service');
 
 module.exports = {
     Customer,
     Ambassador,
-    Referral
 };
--- service/referral.service.js
+++
@@ -1,7 +0,0 @@
-class Referral {
-    static getCountForAllCustomer() {
-
-    }
-}
-
-module.exports = Referral
